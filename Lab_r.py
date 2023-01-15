ea8934f00e8ecbba807b340788e39653848e13b6
Dooha005 <MINGW64@gmail.com~>
2023-01-15 14:17:19 +0300

    .. SOLVED LAb REVIEW ..

---
+++ Lab_r.py
@@ -0,0 +1,20 @@
+# LAB_REVIEW
+## Write a function that takes a string as a parameter:
+
+#3- first should seperate words using split by space (if there is any).
+#- then, it should return the number of words in that string.
+
+# Example: 'fine, Thank you' --> 3
+
+def function (st : str ):   
+    x = st.split()
+    print(x)
+    words = len(st.split())
+    print("Number of words:", words)
+
+function ('fine, Thank you')
+
+
+
+
+
