e0f453e4188b8c1dcccb1e0876c6f29a6b0243f1
nada.abdullah <none.4292@gmail.com>
2023-01-15 14:24:58 +0300

    Lab review are Solved !

---
+++ lesson_review.py
@@ -0,0 +1,19 @@
+# LAB_REVIEW
+## Write a function that takes a string as a parameter:
+#- first should seperate words using split by space (if there is any).
+#- then, it should return the number of words in that string.
+
+#Example: 'fine, Thank you' --> 3
+
+
+def seperate (list_s : str) :
+    sp = list_s.split()
+    word = len(list_s.split())
+    return word  
+
+
+
+
+
+
+print("the number of word is :", seperate("hello my name is nada"))
