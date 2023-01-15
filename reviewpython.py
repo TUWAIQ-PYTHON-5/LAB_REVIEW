2d7aecb60a3ef5950df3d4ce5c3fc7a112b0d82f
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-15 14:54:18 +0300

    solved lab

---
+++ reviewpython.py
@@ -0,0 +1,11 @@
+string1 : str ="How Are You ?"
+def myFunction (string1):
+    temp =''
+    for i in string1 :
+        temp += " " + i
+    else:
+        temp 
+    return temp
+print (myFunction (string1))
+
+print( len(string1.split()))
