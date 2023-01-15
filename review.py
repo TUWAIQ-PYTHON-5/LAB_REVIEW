ed4d932533b161ddfc98cbafad4fffbe68e59ca7
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-15 14:56:01 +0300

    la

---
+++ review.py
@@ -0,0 +1,20 @@
+def spilt(words):
+    return [x for x in words]
+    
+words = 'fine, Thank you'           
+print(spilt(words))
+
+
+
+
+
+
+
+
+
+
+
+def coun(string):
+    return len(string.split())
+print(coun('fine, Thank you'))
+
