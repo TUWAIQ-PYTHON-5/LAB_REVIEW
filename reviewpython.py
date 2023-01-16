8dc8beaaedd7b0300ac81cac853129dc58584e7d
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-16 09:54:49 +0300

    solved lab

--- reviewpython.py
+++ reviewpython.py
@@ -1,11 +1,6 @@
 string1 : str ="How Are You ?"
 def myFunction (string1):
-    temp =''
-    for i in string1 :
-        temp += " " + i
-    else:
-        temp 
-    return temp
+    seperate= string1.rsplit(" ")
+    return seperate
 print (myFunction (string1))
-
 print( len(string1.split()))
